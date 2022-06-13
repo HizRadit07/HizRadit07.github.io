a1d0c2635ca2403c663ed06c48704967c87b4d34
HizRadit07 <hizkiagenius@gmail.com>
2022-06-13 13:14:54 +0700

    added page that calls quotes from quotable.io

--- randomquote.js
+++ randomquote.js
@@ -1,9 +1,13 @@
 $(function (){
     $.ajax({
         type: 'GET',
-        url: 'https://zenquotes.io/api/random',
+        url: 'http://quotable.io/random',
         success: function(data){
             console.log('success', data)
+            document.getElementById("randomquote")
+                .innerHTML = `"${data.content}"`;
+            document.getElementById("randomquote-author")
+                .innerHTML = `- ${data.author}`;
         }
     });
 });
