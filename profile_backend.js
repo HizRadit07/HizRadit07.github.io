1b5cd39ee78ef4a86ce63d3d2901cd230dd29ce9
HizRadit07 <hizkiagenius@gmail.com>
2022-06-20 13:51:06 +0700

    added page that connects directly to backend

--- profile_backend.js
+++ profile_backend.js
@@ -1,12 +1,8 @@
-const auth_header = { //header + api key, in reality, please secure this
-    "Authorization" : "Bearer keyWsVnBWiVw0fLAy"
-}
-
 $(function (){
+    const name = "Hizkia%20Raditya%20Pratama%20Roosadi"
     $.ajax({
         type: 'GET',
-        url: 'https://api.airtable.com/v0/appPucj1FvmbK3LL4/User?maxRecords=1',
-        headers: auth_header,
+        url: `https://my-fastapi-airtable.herokuapp.com/user/name/${name}`,
         success: function(data){
             //this is bad practice, but for the sake of learning,
             //we use only the first data in records, since only my profile is needed
@@ -15,25 +11,15 @@ $(function (){
     });
 });
 
-function filterAboutByID(array, id){
-    for (let i = 0; i < array.length; i++){
-        if (array[i].fields.User[0] == id){
-            return array[i].fields.Description
-        }
-    }
-    return null
-}
-
 $(function (){
+    const name = "Hizkia%20Raditya%20Pratama%20Roosadi"
     $.ajax({
         type: 'GET',
-        url: 'https://api.airtable.com/v0/appPucj1FvmbK3LL4/About?filterByFormula=NOT%28%7BAbout%20ID%7D%20%3D%20%27%27%29',
-        //^ get About where About id != ''
-        headers: auth_header,
+        url: `https://my-fastapi-airtable.herokuapp.com/about/${name}`,
         success: function(data){
             //console.log('success', data)
             //efficiency second, this is just to familiarize me
-            var about = filterAboutByID(data.records, "rec0edkqSx4BJUoXR") //for now still hardcoded my own id here
+            var about = data.records[0].fields.Description //for now still hardcoded my own id here
             document.getElementById('about-description').innerHTML = about
         }
     });
@@ -41,12 +27,11 @@ $(function (){
 
 $(function (){
     var experience = $('#experience_list');
+    const name = "Hizkia%20Raditya%20Pratama%20Roosadi"
 
     $.ajax({
         type: 'GET',
-        url: 'https://api.airtable.com/v0/appPucj1FvmbK3LL4/Experience?filterByFormula=NOT%28%7BExperience%20ID%7D%20%3D%20%27%27%29',
-        //^ get About where About id != ''
-        headers: auth_header,
+        url: `https://my-fastapi-airtable.herokuapp.com/experience/${name}`,
         success: function(data){
             //console.log('success', data.records[0].fields["Company Logo"][0].url)
             //TODO: foreach item in data, append a list item
